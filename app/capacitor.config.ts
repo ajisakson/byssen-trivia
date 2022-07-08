fd2694feb31a5907b706d298fbcb70837787daed
Austin Isakson <austini@crowdhubapps.com>
2022-07-07 20:18:20 -0700

    React and Capacitor config/setup

---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "com.byssen.trivia",
+	appName: "Byssen Trivia",
+	webDir: "build",
+	bundledWebRuntime: false
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,11 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+	res.json({ message: "Please visit /countries to view all the countries" });
 });
 
 app.use("/countries", countryRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
