eea6ca88f3aed013f33af72738b82156ff102957
Austin Isakson <austini@crowdhubapps.com>
2022-07-07 21:06:27 -0700

    fixing dependencies

--- src/routes/country.ts
+++ src/routes/country.ts
@@ -4,35 +4,33 @@ import { CountryModel, ICountry } from "../models/country";
 const routes = Router();
 
 routes.get("/", async (req, res) => {
-  try {
-    const countries: ICountry[] = await CountryModel.find().exec();
-    return res.json(countries);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Sorry, something went wrong :/" });
-  }
+	try {
+		const countries: ICountry[] = await CountryModel.find().exec();
+		return res.json(countries);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: "Sorry, something went wrong :/" });
+	}
 });
 
 routes.post("/", async (req, res) => {
-  try {
-    const country: ICountry = req.body;
+	try {
+		const country: ICountry = req.body;
 
-    const countryExists = await CountryModel.findOne({
-      name: country.name,
-    }).exec();
+		const countryExists = await CountryModel.findOne({
+			name: country.name
+		}).exec();
 
-    if (countryExists) {
-      return res
-        .status(409)
-        .json({ error: "There is already another country with this name" });
-    }
+		if (countryExists) {
+			return res.status(409).json({ error: "There is already another country with this name" });
+		}
 
-    const newCountry = await CountryModel.create(country);
-    return res.status(201).json(newCountry);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Sorry, something went wrong :/" });
-  }
+		const newCountry = await CountryModel.create(country);
+		return res.status(201).json(newCountry);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: "Sorry, something went wrong :/" });
+	}
 });
 
 export default routes;
