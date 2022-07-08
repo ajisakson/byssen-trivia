740fc9686f6bd6bf6b68632c7bce15d370d890e6
Austin Isakson <austini@crowdhubapps.com>
2022-07-07 21:26:28 -0700

    Hooking up the React to the Express!

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import "./lib/db";
-import express from "express";
+import express, { Request, Response } from "express";
 import countryRoutes from "./routes/country";
 
 const app = express();
+const cors = require("cors");
+const path = require("path");
 const port = process.env.PORT || 3333;
 
+const baseUrl = port === 3333 ? path.join(__dirname, "../dist/client") : path.join(__dirname, "client");
+
+app.use(express.static(baseUrl));
+app.use(cors());
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-	res.json({ message: "Please visit /countries to view all the countries" });
+app.get("*", (req: Request, res: Response) => {
+	res.sendFile(path.join(baseUrl, "/index.html"));
 });
 
 app.use("/countries", countryRoutes);
